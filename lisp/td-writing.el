455f958c0d491dce8c9a272fe10f1bb78578c542
tdback <tyler@tdback.net>
2025-02-08 10:11:40 -0500

    lisp/td-writing: fix issues with ispell completion in jinx-mode

--- lisp/td-writing.el
+++ lisp/td-writing.el
@@ -1,11 +1,20 @@
-;;; -*- lexical-binding: t; -*-
+;;; td-writing.el --- writing environment -*- lexical-binding: t; -*-
+
+;;; Code:
 
 (use-package jinx
   :ensure t
   :hook ((org-mode  . jinx-mode)
          (text-mode . jinx-mode))
   :bind (("M-$"   . jinx-correct)
-         ("C-M-$" . jinx-languages)))
+         ("C-M-$" . jinx-languages))
+  :custom
+  ;; https://github.com/minad/corfu/discussions/457
+  (text-mode-ispell-word-completion nil))
+
+(use-package writegood-mode
+  :ensure t
+  :hook (jinx-mode . writegood-mode))
 
 (use-package olivetti
   :ensure t
@@ -15,8 +24,9 @@
   :init
   (setq olivetti-body-width 80))
 
-(use-package writegood-mode
+(use-package markdown-mode
   :ensure t
-  :hook (jinx-mode . writegood-mode))
+  :defer t)
 
 (provide 'td-writing)
+;;; td-writing.el ends here
