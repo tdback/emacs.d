b8ecc7df730bb9e71cb38b5c6d64c20570e1daa4
tdback <tyler@tdback.net>
2025-02-26 19:36:49 -0500

    lisp/td-mu4e: WIP cleanup of module

--- lisp/td-mu4e.el
+++ lisp/td-mu4e.el
@@ -7,6 +7,9 @@
   :bind (("C-c m m" . mu4e)
          ("C-c m c" . 'mu4e-compose-new)
          ("C-c m u" . 'mu4e-update-mail-and-index))
+  :hook ((mu4e-main-mode . (lambda ()
+                             (when (meow-mode)
+                               (meow-insert-mode)))))
   :config
   (setq mu4e-maildir "~/Mail")
 
@@ -29,38 +32,38 @@
   (setq mu4e-view-show-addresses t)
   (setq mu4e-modeline-show-global nil)
 
-  (setq mu4e-contexts
-        (list
-         ;; Personal account.
-         (make-mu4e-context
-          :name "Personal"
-          :vars '((user-mail-address      . "tyler@tdback.net")
-                  (user-full-name         . "Tyler Dunneback")
-                  (mu4e-compose-signature . "Tyler Dunneback")
-                  (mu4e-drafts-folder     . "/tdback/Drafts")
-                  (mu4e-sent-folder       . "/tdback/Sent")
-                  (mu4e-refile-folder     . "/tdback/Archive")
-                  (mu4e-trash-folder      . "/tdback/Trash")))))
+  ;; Email accounts.
+  (setq mu4e-contexts (list
+                       (make-mu4e-context :name "Personal"
+                                          :vars '((user-mail-address      . "tyler@tdback.net")
+                                                  (user-full-name         . "Tyler Dunneback")
+                                                  (mu4e-compose-signature . "Tyler Dunneback")
+                                                  (mu4e-drafts-folder     . "/tdback/Drafts")
+                                                  (mu4e-sent-folder       . "/tdback/Sent")
+                                                  (mu4e-refile-folder     . "/tdback/Archive")
+                                                  (mu4e-trash-folder      . "/tdback/Trash")))))
 
   (setq mu4e-maildir-shortcuts
-        '(("/tdback/Inbox"    . ?i)
-          ("/tdback/Sent"     . ?s)
-          ("/tdback/Trash"    . ?t)
-          ("/tdback/Drafts"   . ?d)
-          ("/tdback/Archive"  . ?a)
-          ("/tdback/All Mail" . ?m))))
+        '((:maildir "/tdback/Inbox"    :key ?i)
+          (:maildir "/tdback/Sent"     :key ?s)
+          (:maildir "/tdback/Trash"    :key ?t)
+          (:maildir "/tdback/Drafts"   :key ?d)
+          (:maildir "/tdback/Archive"  :key ?a)
+          (:maildir "/tdback/All Mail" :key ?m))))
 
+;; TODO: Flesh this part out.
 (use-package org-mime
   :ensure t
-  :config
-  (setq org-mime-export-options (list :section-numbers nil
-                                      :with-author nil
-                                      :with-toc nil))
-  (add-hook 'org-mime-html-hook (lambda ()
-                                  (org-mime-change-element-style
-                                   "pre" (format "color: %s; background-color: %s; padding: 0.5em;"
-                                                 "#E6E1DC" "#232323"))))
-  (add-hook 'message-send-hook 'org-mime-confirm-when-no-multipart))
+  :hook ((org-mime-html-hook . (lambda ()
+                                 (org-mime-change-element-style "pre"
+                                                                (format "color: %s; background-color: %s; padding: 0.5em;"
+                                                                        "#E6E1DC"
+                                                                        "#232323"))))
+         (message-send-hook . org-mime-confirm-when-no-multipart))
+  :custom
+  (org-mime-export-options (list :section-numbers nil
+                                 :with-author nil
+                                 :with-toc nil)))
 
 (provide 'td-mu4e)
 ;;; td-mu4e.el ends here
